0410a1d567b1f235941536a553509095321d7b87
angeel22m <mmoises.martinez0139@gmail.com>
2025-09-12 16:25:29 -0600

    nuevos archivos: backend/.env.example 	modificados:     frontend/index.html 	modificados:     frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import PacientesList from "./componentes/PacientesList";
 function App() {
   return (
     <div>
-      <h1>Clínica Odontológica</h1>
+      <h1>Prueba api Get</h1>
       <PacientesList />
     </div>
   );
